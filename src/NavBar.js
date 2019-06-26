5a4abc9859655bb2ea37d65271ff81cf36f8e461
kivvvvv <kivistheslayer@gmail.com>
2019-06-26 21:20:48 +0700

    13# feat: Added Drawer on NavBar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, animateScroll as scroll } from "react-scroll";
 import Container from "@material-ui/core/Container";
+import IconButton from "@material-ui/core/IconButton";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
 import makeStyles from "@material-ui/core/styles/makeStyles";
+import MenuIcon from "mdi-material-ui/Menu";
+import ChevronLeftIcon from "mdi-material-ui/ChevronLeft";
+import HomeIcon from "mdi-material-ui/Home";
+import MonitorIcon from "mdi-material-ui/Monitor";
+import GmailIcon from "mdi-material-ui/Gmail";
 
 import LiquidCheeseSvg from "./Liquid-Cheese.svg";
 
@@ -22,20 +34,114 @@ const useStyles = makeStyles(theme => ({
   },
   button: {
     margin: theme.spacing(1)
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  drawerHeader: {
+    display: "flex",
+    alignItems: "center",
+    padding: "0 8px",
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end"
+  },
+  list: {
+    width: 250
+  },
+  fullList: {
+    width: "auto"
   }
 }));
 
 export default function NavBar() {
   const classes = useStyles();
 
+  const [openDrawer, setOpenDrawer] = useState(false);
+
   const scrollToTop = () => {
     scroll.scrollToTop();
+    setOpenDrawer(false);
   };
+
+  const handleToggleDrawer = e => {
+    if (e.type === "keydown" && (e.key === "Tab" || e.key === "Shift")) {
+      return;
+    }
+
+    setOpenDrawer(!openDrawer);
+  };
+
+  const sideList = () => (
+    <div
+      className={classes.list}
+      role="presentation"
+      onClick={null}
+      onKeyDown={null}
+    >
+      <List>
+        <ListItem button onClick={scrollToTop}>
+          <ListItemIcon>
+            <HomeIcon />
+          </ListItemIcon>
+          <ListItemText primary="Home" />
+        </ListItem>
+        <Link
+          activeClass="active"
+          to="projects"
+          spy={true}
+          offset={-64}
+          smooth={true}
+          duration={500}
+          onClick={() => setOpenDrawer(!openDrawer)}
+        >
+          <ListItem button>
+            <ListItemIcon>
+              <MonitorIcon />
+            </ListItemIcon>
+            <ListItemText primary="Projects" />
+          </ListItem>
+        </Link>
+        <Link
+          activeClass="active"
+          to="getInTouch"
+          spy={true}
+          smooth={true}
+          duration={500}
+          onClick={() => setOpenDrawer(!openDrawer)}
+        >
+          <ListItem button>
+            <ListItemIcon>
+              <GmailIcon />
+            </ListItemIcon>
+            <ListItemText primary="Get in Touch" />
+          </ListItem>
+        </Link>
+      </List>
+    </div>
+  );
 
   return (
     <AppBar position="fixed" className={classes.appBar}>
+      <Drawer open={openDrawer} onClose={handleToggleDrawer}>
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleToggleDrawer}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <Divider />
+        {sideList()}
+      </Drawer>
       <Container maxWidth="md">
         <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="Menu"
+            onClick={handleToggleDrawer}
+          >
+            <MenuIcon />
+          </IconButton>
           <Typography className={classes.home}>Sirapop</Typography>
           <Button
             variant="outlined"
